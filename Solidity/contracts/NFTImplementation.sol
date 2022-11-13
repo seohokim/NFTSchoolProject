5746938da3a3d28fbea09c66be8f77e9b5fb7734
JeremyHaechi <jeremy@haechi.io>
2022-11-14 04:55:13 +0900

    [Solidity] minting and burning implementation (half)

--- Solidity/contracts/NFTImplementation.sol
+++ Solidity/contracts/NFTImplementation.sol
@@ -17,6 +17,9 @@ contract NFTImplementation is INFTImplementation, AccessControl, ERC721("OurNFT"
     Counters.Counter public currentTokenID;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER");
     bytes32 public constant BURNER_ROLE = keccak256("BURNER");
+    uint256 public constant MAXIMUM_TOKEN_ID = 10000000000000000000000;
+
+    mapping(address => TokenMetadata) private metadata;
 
     // Modifiers and Events
     modifier onlyAdmin(address sender) {
@@ -44,13 +47,50 @@ contract NFTImplementation is INFTImplementation, AccessControl, ERC721("OurNFT"
     // 1. Basic Minting, Burning implementation
     // 2. Basic Transferring implementation
     // 3. Auction implementation
-    function mint() external onlyMinter(msg.sender) returns(bool) {
+    function mint(address user, MetaData calldata data) external onlyMinter(msg.sender) returns(bool) {
         // Minting with corresponding initialize fee (0.00001 ether)
-        
+        // Just for real network deploying, not development phase
+        // require(msg.value >= 0.00001 ether, "Need at least 0.00001 ether for minting");
+        // First, check this is first minting on target user
+        TokenMetadata storage metaData = metadata[user];
+        if (metaData.owner == address(0)) {
+            // This is first minting process
+            metaData.owner = address(user);
+        }
+        require(metaData.ids[data.unique_id] != true, "That's unique_id already exists");
+        metaData.ids[data.unique_id] = true;
+        metaData.stored.push(data);
+        return true;
     }
 
-    function burn() external onlyBurner(msg.sender) returns(bool) {
+    function burn(address user, uint256 unique_id) external returns(bool) {
+        require(msg.sender == user, "You can't burn this token");
+        TokenMetadata storage metaData = metadata[user];
+        
+        if (metaData.owner == address(0)) {
+            return false;
+        }
+        require(metaData.ids[unique_id] == true, "This id is not exists");
+        uint256 tokenIndex = _findTokenID(metaData, unique_id);
 
+        if (tokenIndex == uint256(MAXIMUM_TOKEN_ID)) {
+            return false;
+        } else {
+            metaData.ids[unique_id] = false;
+            metaData.stored[tokenIndex] = metaData.stored[metaData.stored.length - 1];
+            metaData.stored.pop();
+        }
+        return true;
+    }
+
+    // private functions
+    function _findTokenID(TokenMetadata storage _metadata, uint256 unique_id) private view returns (uint256) {
+        for (uint256 i = 0; i < _metadata.stored.length; i++) {
+            if (_metadata.stored[i].unique_id == unique_id) {
+                return i;
+            }
+        }
+        return uint256(MAXIMUM_TOKEN_ID);
     }
 
     // Admin Functions
--- Solidity/contracts/interfaces/INFTImplementation.sol
+++ Solidity/contracts/interfaces/INFTImplementation.sol
@@ -2,5 +2,20 @@
 pragma solidity ^0.8.9;
 
 interface INFTImplementation {
-    
+    struct MetaData {
+        uint256 unique_id;
+    }
+
+    struct TokenMetadata {
+        address owner;
+        MetaData[] stored;
+        mapping(uint256 => bool) ids;
+    }
+
+    function mint(address user, MetaData calldata data) external returns(bool);
+    function burn(address user, uint256 unique_id) external returns(bool);
+
+    function changeOwner(address user) external;
+    function addMinter(address minter) external;
+    function removeMinter(address minter) external;
 }
