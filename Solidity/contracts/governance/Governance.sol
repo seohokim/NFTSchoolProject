68080a4a4efc7a58408e418ab40842bd645097cd
JeremyHaechi <jeremy@haechi.io>
2022-11-17 14:57:44 +0900

    Solidity - update

--- Solidity/contracts/governance/Governance.sol
+++ Solidity/contracts/governance/Governance.sol
@@ -4,6 +4,39 @@ pragma solidity ^0.8.9;
 import "../libs/DataTypes.sol";
 import "../interfaces/IGovernance.sol";
 
-contract Governance is IGovernance {
-    
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Governance is IGovernance, Ownable {
+    address private governanceOwner;
+    address private nftCore;
+
+    mapping(address => uint256) banList;
+
+    constructor(address owner, address core) {
+        governanceOwner = owner;
+        nftCore = core;
+
+        _initializeOwnership(governanceOwner);
+    }
+
+    function _initializeOwnership(address newOwner) private {
+        _transferOwnership(newOwner);
+    }
+
+    function ban(address user, uint256 period) external returns (bool) {
+        require(period > 0, "PERIOD must be greater than 0");
+        banList[user] = block.timestamp + period;
+        return true;
+    }
+
+    function unban(address user) external returns (bool) {
+        require(block.timestamp >= banList[user], "Can't unban yet");
+        delete banList[user];
+
+        return true;
+    }
+
+    function emergency(address user) external onlyOwner {
+        delete banList[user];
+    }
 }
--- Solidity/contracts/interfaces/IGovernance.sol
+++ Solidity/contracts/interfaces/IGovernance.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.8.9;
 import "../libs/DataTypes.sol";
 
 interface IGovernance {
-    
+    function ban(address user, uint256 period) external returns (bool);
+    function unban(address user) external returns (bool);
 }
