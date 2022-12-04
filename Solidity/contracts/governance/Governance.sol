cf1de566a1ed66c44487bc5c10043ff18dc05f93
JeremyHaechi <jeremy@haechi.io>
2022-12-04 17:10:25 +0900

    [UPDATE] Governance Permission Updated

--- Solidity/contracts/governance/Governance.sol
+++ Solidity/contracts/governance/Governance.sol
@@ -3,16 +3,30 @@ pragma solidity ^0.8.9;
 
 import "../libs/DataTypes.sol";
 import "../interfaces/IGovernance.sol";
+import "../utils/OwnableCustom.sol";
+
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Governance is IGovernance, Ownable {
+contract Governance is IGovernance, OwnableCustom {
     address private governanceOwner;
     address private nftCore;
 
     mapping(address => uint256) banList;
     Reported[] reportedQueue;
     mapping(address => uint256) reportedCounter;
+
+    bytes32 public constant BAN_ADMIN = keccak256("POLICE");
+
+    modifier onlyPolice(address caller) {
+        require(hasRole(BAN_ADMIN, caller), "YOU ARE NOT POLICE");
+        _;
+    }
+
+    modifier onlyInternal(address caller) {
+        require(hasRole(DEFAULT_ADMIN_ROLE, caller) || hasRole(ALLOW_ROLE, caller), "NOT ALLOWED");
+        _;
+    }
 
     constructor(address owner, address core) {
         governanceOwner = owner;
@@ -22,43 +36,45 @@ contract Governance is IGovernance, Ownable {
     }
 
     function _initializeOwnership(address newOwner) private {
-        _transferOwnership(newOwner);
+        _setupRole(DEFAULT_ADMIN_ROLE, newOwner);
+        _setupRole(DEFAULT_ADMIN_ROLE, address(this));          // Allowed from myself
+        _setupRole(BAN_ADMIN, newOwner);
+
+        // Contract allowing
+        require(nftCore != address(0), "YOU HAVE TO SET CORE CONTRACT FIRST");
+        addAllowedContract(nftCore);
     }
 
-    function ban(address user, uint256 period) external returns (bool) {
+    function ban(address user, uint256 period) external onlyPolice(msg.sender) returns (bool) {
         require(period > 0, "PERIOD must be greater than 0");
         banList[user] = block.timestamp + period;
         return true;
     }
 
-    function unban(address user) external returns (bool) {
+    function unban(address user) external onlyPolice(msg.sender) returns (bool) {
         require(block.timestamp >= banList[user], "Can't unban yet");
         delete banList[user];
 
         return true;
     }
 
-    function emergency(address user) external onlyOwner {
+    function emergency(address user) external onlyAdmin(msg.sender) {
         delete banList[user];
     }
 
     function report(address from, address reported, bytes memory description) external payable {
-        uint256 BASE_FEE = 0.0001 ether;
-        uint256 realFee = BASE_FEE + (0.000001 ether * reportedCounter[from]);
-
-        require(msg.value >= realFee, "Your fee is too low");
         reportedQueue.push(Reported(
             from, reported, description
         ));
     }
 
-    function out() external returns (Reported memory) {
+    function out() external onlyInternal(msg.sender) returns (Reported memory) {
         Reported memory value = reportedQueue[reportedQueue.length - 1];
         reportedQueue.pop();
         return value;
     }
 
-    function popReportingCounter(address user) external returns(uint256) {
+    function popReportingCounter(address user) external onlyInternal(msg.sender) returns(uint256) {
         uint256 poped = reportedCounter[user];
         if (poped == 0)
             return 0;
@@ -67,7 +83,7 @@ contract Governance is IGovernance, Ownable {
         return poped;
     }
 
-    function giveReward(address user) external {
+    function giveReward(address user) external onlyAdmin(msg.sender) {
         reportedCounter[user] = reportedCounter[user] + 1;
     }
 }
