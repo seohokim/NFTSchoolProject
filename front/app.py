76be7c1f228cc6d92fa01fd4937e979cf0f3e66e
Ajou-sipdo <118275773+Ajou-sipdo@users.noreply.github.com>
2022-12-06 14:49:20 +0900

    Add files via upload

--- front/app.py
+++ front/app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, render_template, redirect, url_for, abort
 
 app =Flask(__name__)
 
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
 @app.route('/')
 def hello():
     return 'Hello, World!'
@@ -10,93 +14,55 @@ def hello():
 def login():
     return render_template("login.html")
 
-@app.route('/userHome')
-def userHome():
-    return render_template("userHome.html")
+@app.route('/login/walletPopup')
+def walletPopup():
+    return render_template("walletPopup.html")
+
+@app.route('/home/patentApp')
+def patentApp():
+    return render_template("patentApp.html")
+
+@app.route('/home/patentApp/cancelPopup')
+def cancelPopup():
+    return render_template("cancelPopup.html")
+
+@app.route('/home/patentManagement')
+def patentManagement():
+    return render_template("patentManagement.html")
     
-@app.route('/userHome/myinfo')
-def myinfo():
-    return render_template("myInfo.html")
+@app.route('/home/patentManagement/PatentManView')
+def PatentManView():
+    return render_template("PatentManView.html")
 
-@app.route('/userHome/myTokenManage')
-def myTokenManage():
-    return render_template("myTokenManage.html")
-
-@app.route('/userHome/exchangeHome')
-def exchangeHome():
-    return render_template("exchangeHome.html")
-
-@app.route('/userHome/myTokenManage/burnRequest')
-def burnRequest():
-    return render_template("burRequest.html")
-
-@app.route('/userHome/myTokenManage/recoverRequest')
-def recoverRequest():
-    return render_template("recoverRequest.html")
-
-@app.route('/userHome/exchangeHome/auction')
+@app.route('/home/auction')
 def auction():
     return render_template("auction.html")
 
-@app.route('/userHome/exchangeHome/market')
-def market():
-    return render_template("market.html")
+@app.route('/home/auction/auctionSell')
+def auctionSell():
+    return render_template("auctionSell.html")
 
-@app.route('/userHome/exchangeHome/market/nowOnAuction')
-def nowOnAuctionList():
-    return render_template("nowOnAuctionList.html")
+@app.route('/home/auction/auctionView')
+def auctionView():
+    return render_template("auctionView.html")
 
-@app.route('/userHome/exchangeHome/market/myAuctionStatus')
-def myAuctionStatus():
-    return render_template("myAuctionStatus.html")
+@app.route('/home/setting')
+def setting():
+    return render_template("setting.html")
 
-# @app.route('/')
-# def ():
-#     return render_template(".html")
+@app.route('/adminHome')
+def adminHome():
+    return render_template("adminHome.html")
 
-# @app.route('/')
-# def ():
-#     return render_template(".html")
+@app.route('/adminHome/adminExamination')
+def adminExamination():
+    return render_template("adminExamination.html")
 
-# @app.route('/')
-# def ():
-#     return render_template(".html")
-
-# @app.route('/')
-# def ():
-#     return render_template(".html")
-
-# @app.route('/')
-# def ():
-#     return render_template(".html")
-
-# @app.route('/')
-# def ():
-#     return render_template(".html")
-
-# @app.route('/')
-# def ():
-#     return render_template(".html")
-
-# @app.route('/')
-# def ():
-#     return render_template(".html")
-
-# @app.route('/')
-# def ():
-#     return render_template(".html")
-
-# @app.route('/')
-# def ():
-#     return render_template(".html")
+@app.route('/adminHome/adminExamView')
+def adminExamView():
+    return render_template("adminExamView.html")
     
-# @app.route('/')
-# def ():
-#     return render_template(".html")
 
-# @app.route('/')
-# def ():
-#     return render_template(".html")
 
 if __name__ == '__main__':
     app.run(debug=True)
